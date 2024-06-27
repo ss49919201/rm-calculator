a6964c2861454f0abb5e1c245666dd2addc8002d
ss49919201 <sakashin10291029@gmail.com>
2024-06-27 22:12:59 +0900

    feat: call calc rm workflow from entry point

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,41 @@
 import { OptionValues, program } from "commander";
+import { calcRm } from "./workflow/calcRm";
 
 type RunInput = {
   dryRun: boolean;
+  rep: number;
+  weight: number;
 };
 
 function run(input: RunInput) {
-  console.log("Hello World!");
-  console.log("Input: ", input);
+  const out = calcRm({
+    rep: input.rep,
+    weight: input.weight,
+  });
+
+  console.log(`1RM: ${out.rm}`);
 }
 
 function optionsToRunInput(option: OptionValues): RunInput {
   return {
     dryRun: option.dryRun,
+    rep: option.rep,
+    weight: option.weight,
   };
 }
 
-program.option("-d, --dry-run", "dry run");
-program.parse();
+function parseFloatOrThrow(value: string) {
+  const f = parseFloat(value);
+  if (isNaN(f)) {
+    throw new Error("Invalid number");
+  }
+  return f;
+}
+
+program
+  .requiredOption("-r, --rep <rep>", "rep", parseFloatOrThrow)
+  .requiredOption("-w, --weight <weight>", "weight", parseFloatOrThrow)
+  .option("-d, --dry-run", "dry run")
+  .parse();
+
 run(optionsToRunInput(program.opts()));
